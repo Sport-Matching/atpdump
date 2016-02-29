a96ef6bc4c5ae2be42dfaa84de430e5f6d6c5650
Robin THONI <robin@rthoni.com>
2016-02-29 13:13:38 +0100

    get player basic infos

--- atpworldtour.py
+++ atpworldtour.py
@@ -1,1 +1,59 @@
 #! /usr/bin/env python3
+
+import re
+import datetime
+from lxml import html
+import requests
+session = requests.session()
+
+
+baseUrl = 'http://www.atpworldtour.com'
+getRankingUrl = baseUrl + '/en/rankings/singles/?rankDate=2016-2-29&countryCode=all&rankRange=0-1'
+
+allPlayersHtml = session.get(getRankingUrl)
+allPlayersTree = html.fromstring(allPlayersHtml.text)
+allPlayersTreeElements = allPlayersTree.xpath("/html/body/div[@id='mainLayoutWrapper']/div[@id='backbonePlaceholder']"
+                                              "/div[@id='mainContainer']/div[@id='mainContent']"
+                                              "/div[@id='singlesRanking']/div[@id='rankingDetailAjaxContainer']"
+                                              "/table[@class='mega-table']/tbody/tr")
+
+for playerTreeElement in allPlayersTreeElements:
+    nameElement = playerTreeElement[3][0]
+    playerUrl = baseUrl + nameElement.attrib['href']
+    playerHtml = session.get(playerUrl)
+    playerTree = html.fromstring(playerHtml.text)
+
+    e = playerTree.xpath("/html/body/div[@id='mainLayoutWrapper']/div[@id='backbonePlaceholder']"
+                         "/div[@id='mainContainer']/div[@id='mainContent']/div[@id='playerProfileHero']"
+                         "/div[@class='player-profile-hero-overflow']/div[@class='player-profile-hero-table']"
+                         "/div[@class='inner-wrap']/table/tr[1]/td[1]/div[@class='wrap']"
+                         "/div[@class='table-big-value']/span[@class='table-birthday-wrapper']/span")
+    birthDateString = e[0].text.strip()
+    birthDate = datetime.datetime.strptime(birthDateString, "(%Y.%m.%d)")
+
+    e = playerTree.xpath("/html/body/div[@id='mainLayoutWrapper']/div[@id='backbonePlaceholder']"
+                         "/div[@id='mainContainer']/div[@id='mainContent']/div[@id='playerProfileHero']"
+                         "/div[@class='player-profile-hero-overflow']/div[@class='player-profile-hero-dash']"
+                         "/div[@class='inner-wrap']/div[@class='player-profile-hero-ranking']"
+                         "/div[@class='player-flag']/div[@class='player-flag-code']")
+    country = e[0].text.strip()
+
+    e = playerTree.xpath("/html/body/div[@id='mainLayoutWrapper']/div[@id='backbonePlaceholder']"
+                         "/div[@id='mainContainer']/div[@id='mainContent']/div[@id='playerProfileHero']"
+                         "/div[@class='player-profile-hero-overflow']/div[@class='player-profile-hero-table']"
+                         "/div[@class='inner-wrap']/table/tr[1]/td[3]/div[@class='wrap']"
+                         "/div[@class='table-big-value']/span[@class='table-weight-kg-wrapper']")
+    weight = e[0].text.strip()
+    m = re.search("\(([0-9]+)kg\)", weight)
+    weight = int(m.group(1)) * 1000
+
+    e = playerTree.xpath("/html/body/div[@id='mainLayoutWrapper']/div[@id='backbonePlaceholder']"
+                         "/div[@id='mainContainer']/div[@id='mainContent']/div[@id='playerProfileHero']"
+                         "/div[@class='player-profile-hero-overflow']/div[@class='player-profile-hero-table']"
+                         "/div[@class='inner-wrap']/table/tr[1]/td[4]/div[@class='wrap']"
+                         "/div[@class='table-big-value']/span[@class='table-height-cm-wrapper']")
+    length = e[0].text.strip()
+    m = re.search("\(([0-9]+)cm\)", length)
+    length = int(m.group(1))
+
+    print(birthDate, country, weight, length)
